750ce3009c0de51dfc906871abe1fe548e0fc6d2
snowcat-Jung <itct15@itct15ui-iMac.local>
2015-12-04 11:44:11 +0900

    app

---
+++ Snowcat(SeungHwan)Jung/Art_In/Art_In/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Art_In
+//
+//  Created by MAC-snowcat on 2015. 12. 3..
+//  Copyright © 2015년 MAC-snowcat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
